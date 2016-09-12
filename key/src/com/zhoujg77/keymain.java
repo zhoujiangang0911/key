a876b97786874ac6a09b3a81290923a25334955b
zhoujiangang <zhoqjg77@163.com>
2016-09-12 23:41:43 +0800

    解决不能模拟问题

--- key/src/com/zhoujg77/keymain.java
+++ key/src/com/zhoujg77/keymain.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Robot;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 
 public class keymain {
@@ -18,15 +19,15 @@ public class keymain {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		robot.keyPress(KeyEvent.VK_ALT);
-//		robot.keyPress(KeyEvent.VK_TAB);
-//		robot.keyRelease(KeyEvent.VK_ALT);
-//		robot.keyRelease(KeyEvent.VK_TAB);
-//		robot.delay(500);
+		robot.keyPress(KeyEvent.VK_ALT);
+		robot.keyPress(KeyEvent.VK_TAB);
+		robot.keyRelease(KeyEvent.VK_ALT);
+		robot.keyRelease(KeyEvent.VK_TAB);
+		robot.delay(500);
 		int a= 0;
 		while(true){
 			a++;
-			if(a==1){
+			if(a==10){
 				System.exit(0);
 			}
 			//Thread.sleep(2000);
@@ -36,244 +37,117 @@ public class keymain {
 	      //  if(color.getRed()==102&&color.getGreen()==0&&color.getBlue()==255){
 	        //	System.out.println("鼠标在蓝色停留了第"+a+"次");
 	       // }
-	        	robot.mouseMove(680, 448);
-	    		robot.mousePress(KeyEvent.BUTTON1_MASK);
-	    		robot.mouseRelease(KeyEvent.BUTTON1_MASK);
-	    		robot.mousePress(KeyEvent.BUTTON1_MASK);
-	    		robot.mouseRelease(KeyEvent.BUTTON1_MASK);
-	    		robot.delay(500);
+//	        	robot.mouseMove(680, 448);
+//	    		robot.mousePress(KeyEvent.BUTTON1_MASK);
+//	    		robot.mouseRelease(KeyEvent.BUTTON1_MASK);
+//	    		robot.mousePress(KeyEvent.BUTTON1_MASK);
+//	    		robot.mouseRelease(KeyEvent.BUTTON1_MASK);
+//	    		robot.delay(500);
+//	    		
+//	    		robot.keyPress(KeyEvent.VK_F1);
+//	    		robot.keyRelease(KeyEvent.VK_F1);
+//	    		robot.delay(1600);
+//	    		robot.delay(1600);
+//	    		robot.keyPress(KeyEvent.VK_F2);
+//	    		robot.keyRelease(KeyEvent.VK_F2);
+//	    		robot.delay(1600);
 	    		
-	    		robot.keyPress(KeyEvent.VK_F1);
-	    		robot.keyRelease(KeyEvent.VK_F1);
-	    		robot.delay(1500);
-	    		robot.keyPress(KeyEvent.VK_F2);
-	    		robot.keyRelease(KeyEvent.VK_F2);
-	    		robot.delay(1500);
-	    	
-	    		
+	    		//table(robot,a);
+				//robot.mouseMove(10, 20);//移动鼠标到(10,20)点
+
+			    robot.mousePress(InputEvent.BUTTON3_MASK);//按下右键
+			    //robot.delay(4000);
+			    robot.mouseMove(100, 200);
+			    robot.mouseRelease(InputEvent.BUTTON3_MASK);
+			    robot.delay(1600);
+//	    		robot.keyPress(KeyEvent.VK_F3);
+//	    		robot.keyRelease(KeyEvent.VK_F3);
+//	    		robot.delay(1600);
+//	    		table(robot,a);
+//	    		robot.keyPress(KeyEvent.VK_F4);
+//	    		robot.keyRelease(KeyEvent.VK_F4);
+//	    		robot.delay(1600);
+//	    		table(robot,a);
+//	    		robot.keyPress(KeyEvent.VK_F5);
+//	    		robot.keyRelease(KeyEvent.VK_F5);
+//	    		robot.delay(1600);
+//	    	
+//	    		robot.keyPress(KeyEvent.VK_F6);
+//	    		robot.keyRelease(KeyEvent.VK_F6);
+//	    		robot.delay(1600);
+//	    	
+//	    		robot.keyPress(KeyEvent.VK_F7);
+//	    		robot.keyRelease(KeyEvent.VK_F7);
+//	    		robot.delay(1600);
+//	    		robot.delay(1600);
+//	    		robot.keyPress(KeyEvent.VK_F8);
+//	    		robot.keyRelease(KeyEvent.VK_F8);
+//	    		robot.delay(1600);
+//	    	
+//	    		robot.keyPress(KeyEvent.VK_F9);
+//	    		robot.keyRelease(KeyEvent.VK_F9);
+//	    		robot.delay(1600);
+//	    	
+//	    		robot.keyPress(KeyEvent.VK_F10);
+//	    		robot.keyRelease(KeyEvent.VK_F10);
+//	    		robot.delay(1600);
+//	    	
+//	    		robot.keyPress(KeyEvent.VK_F11);
+//	    		robot.keyRelease(KeyEvent.VK_F11);
+//	    		robot.delay(1600);
+//	    	
+//	    		robot.keyPress(KeyEvent.VK_F12);
+//	    		robot.keyRelease(KeyEvent.VK_F12);
+//	    		robot.delay(1600);
 	    		
 		}
 		
 		
 		
-		//这里是按下和释放alt键，这个键的功能是调用菜单
-	/*	
-		robot.keyPress(KeyEvent.VK_ALT);
-		robot.keyRelease(KeyEvent.VK_ALT);
-		robot.delay(500);//延迟500ms
-		//这里是按下和释放回车键，用于确定是file选项
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		robot.delay(500);
-		//选择New
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		robot.delay(500);
-		//选择Java Project
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		robot.delay(500);
 		
-		//输入字母a,即工程名
-		robot.keyPress(KeyEvent.VK_A);
-		robot.keyRelease(KeyEvent.VK_A);
-		robot.delay(500);
-		
-		//回车确定创建工程
-		robot.keyPress(KeyEvent.VK_ENTER);		
-		robot.keyRelease(KeyEvent.VK_ENTER);		
-		robot.delay(500);		
-		//将鼠标抵用到刚刚建立的工程上面，这个坐标是作者在自己的电脑上实验获得的，在不同的电脑上可能不一样，可以用
-		//Point point = MouseInfo.getPointerInfo().getLocation();
-		//System.out.println(point);来打印当前鼠标的坐标，从而找到对的位置
-		robot.mouseMove(43, 136);
-		//按下和释放鼠标左键，选定工程
-		robot.mousePress(KeyEvent.BUTTON1_MASK);
-		robot.mouseRelease(KeyEvent.BUTTON1_MASK);
+	}
 
-		//alt键选择菜单并调出newClass的选项
-		robot.keyPress(KeyEvent.VK_ALT);
-		robot.keyRelease(KeyEvent.VK_ALT);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_DOWN);
-		robot.keyRelease(KeyEvent.VK_DOWN);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_DOWN);
-		robot.keyRelease(KeyEvent.VK_DOWN);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_DOWN);
-		robot.keyRelease(KeyEvent.VK_DOWN);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		robot.delay(500);
-		//下面换回小写
-		//切换成大写
-		robot.keyPress(KeyEvent.VK_CAPS_LOCK);
-		robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_H);
-		robot.keyRelease(KeyEvent.VK_H);
-		robot.delay(200);
-		//换回小写
-		robot.keyPress(KeyEvent.VK_CAPS_LOCK);
-		robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_E);
-		robot.keyRelease(KeyEvent.VK_E);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_L);
-		robot.keyRelease(KeyEvent.VK_L);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_L);
-		robot.keyRelease(KeyEvent.VK_L);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_O);
-		robot.keyRelease(KeyEvent.VK_O);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_CAPS_LOCK);
-		robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_W);
-		robot.keyRelease(KeyEvent.VK_W);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_CAPS_LOCK);
-		robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_O);
-		robot.keyRelease(KeyEvent.VK_O);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_R);
-		robot.keyRelease(KeyEvent.VK_R);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_L);
-		robot.keyRelease(KeyEvent.VK_L);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_D);
-		robot.keyRelease(KeyEvent.VK_D);
-		robot.delay(200);
+	private static void table(Robot robot2,int a) {
 		
-		//移动鼠标选择自动穿件main函数
-		robot.mouseMove(533, 448);
-		robot.mousePress(KeyEvent.BUTTON1_MASK);
-		robot.mouseRelease(KeyEvent.BUTTON1_MASK);
-		robot.delay(500);
-		//回车创建完成
-		robot.keyPress(KeyEvent.VK_ENTER);
-		robot.keyRelease(KeyEvent.VK_ENTER);
-		robot.delay(500);
-		//移动光标到输入代码的行上
-		robot.keyPress(KeyEvent.VK_DOWN);
-		robot.keyRelease(KeyEvent.VK_DOWN);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_DOWN);
-		robot.keyRelease(KeyEvent.VK_DOWN);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_DOWN);
-		robot.keyRelease(KeyEvent.VK_DOWN);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_DOWN);
-		robot.keyRelease(KeyEvent.VK_DOWN);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_DOWN);
-		robot.keyRelease(KeyEvent.VK_DOWN);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_DOWN);
-		robot.keyRelease(KeyEvent.VK_DOWN);
-		robot.delay(500);
+		keyboard(robot,KeyEvent.VK_KP_LEFT,2);
+		if(a%2==0){
+			keyboard(robot,KeyEvent.VK_KP_RIGHT,2);
+		}
+	}
 
-		//输入syso并用alt+/快捷键补全
-		robot.keyPress(KeyEvent.VK_S);
-		robot.keyRelease(KeyEvent.VK_S);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_Y);
-		robot.keyRelease(KeyEvent.VK_Y);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_S);
-		robot.keyRelease(KeyEvent.VK_S);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_O);
-		robot.keyRelease(KeyEvent.VK_O);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_ALT);
-		robot.keyPress(KeyEvent.VK_SLASH);
-		robot.keyRelease(KeyEvent.VK_ALT);
-		robot.keyRelease(KeyEvent.VK_SLASH);
-		robot.delay(500);
-
-		//输入双引号
-		robot.keyPress(KeyEvent.VK_SHIFT);
-		robot.keyPress(KeyEvent.VK_QUOTE);
-		robot.keyRelease(KeyEvent.VK_SHIFT);
-		robot.keyRelease(KeyEvent.VK_QUOTE);
-
-		//输入HelloWorld
-		robot.keyPress(KeyEvent.VK_CAPS_LOCK);
-		robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
-		robot.delay(500);
-		robot.keyPress(KeyEvent.VK_H);
-		robot.keyRelease(KeyEvent.VK_H);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_CAPS_LOCK);
-		robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_E);
-		robot.keyRelease(KeyEvent.VK_E);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_L);
-		robot.keyRelease(KeyEvent.VK_L);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_L);
-		robot.keyRelease(KeyEvent.VK_L);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_O);
-		robot.keyRelease(KeyEvent.VK_O);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_CAPS_LOCK);
-		robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_W);
-		robot.keyRelease(KeyEvent.VK_W);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_CAPS_LOCK);
-		robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_O);
-		robot.keyRelease(KeyEvent.VK_O);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_R);
-		robot.keyRelease(KeyEvent.VK_R);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_L);
-		robot.keyRelease(KeyEvent.VK_L);
-		robot.delay(200);
-		robot.keyPress(KeyEvent.VK_D);
-		robot.keyRelease(KeyEvent.VK_D);
-		robot.delay(200);
-
-		//ctrl+shift+F 格式化
-		robot.keyPress(KeyEvent.VK_SHIFT);
-		robot.keyPress(KeyEvent.VK_CONTROL);
-		robot.keyPress(KeyEvent.VK_F);
-		robot.keyRelease(KeyEvent.VK_SHIFT);
-		robot.keyRelease(KeyEvent.VK_CONTROL);
-		robot.keyRelease(KeyEvent.VK_F);
-		
-		//ctrl+S保存
-		robot.keyPress(KeyEvent.VK_CONTROL);
-		robot.keyPress(KeyEvent.VK_S);
-		robot.keyRelease(KeyEvent.VK_S);
-		robot.keyRelease(KeyEvent.VK_CONTROL);
-		
-		//F11运行
-		robot.keyPress(KeyEvent.VK_F11);
-		robot.keyRelease(KeyEvent.VK_F11);	*/	
-		
+	private static void keyboard(Robot robot2, int vkLeft,int num) {
+		int a = 0;
+		while(true){
+			System.out.println("当前按键："+vkLeft+"次数："+num);	
+			
+			if(a==num){
+				break;
+			}
+			System.out.println(a);
+			robot.keyPress(vkLeft);
+			robot.keyRelease(vkLeft);
+			robot.delay(500); 
+			a++;
+		}
 	}	
+	
+	
+	private static void keyboard2(Robot robot2, int v1,int v2,int num) {
+		int a = 0;
+		while(true){
+			
+			if(a==num){
+				break;
+			}
+			System.out.println(a);
+			robot.keyPress(v1);
+			robot.keyPress(v2);
+			robot.keyRelease(v1);
+			robot.keyRelease(v2);
+			robot.delay(500); 
+			a++;
+		}
+	}	
+	
+	
 }
